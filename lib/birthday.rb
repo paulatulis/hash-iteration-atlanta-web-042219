a14b344dcb5d39093daf27c918bd18b8ce226554
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 18:16:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,17 +5,5 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-  puts "Happy Birthday #{name}! You are now #{age} years old!"
-end
-end
 
-
- winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-  end
-winner
 end
